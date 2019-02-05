f5241403c590f3c1a08140014038a2129d9613ec
Fredson Rodrigues <fredson.jorge@ceuma.br>
2019-02-05 15:21:29 -0300

    Remodelando tela de resultado

--- front-end/src/Pages/FormResult.js
+++ front-end/src/Pages/FormResult.js
@@ -1,25 +1,26 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import { Card, Button } from "../Components";
-import { restartSimulation } from '../Actions/FormActions'
+import { restartSimulation } from "../Actions/FormActions";
+import cellphones from "../Assets/img/cel.png";
+import "../Assets/img.css";
 
 class FormResult extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      simulation : this.props.simulation
-    }
+      simulation: this.props.simulation
+    };
+    this.image = cellphones;
   }
-  
 
   componentWillReceiveProps(nextProps) {
-    this.setState(nextProps.simulation)
+    this.setState(nextProps.simulation);
   }
 
   ListCard = () => {
-    return this.state.simulation.map(s => 
-      <div className="column is-one-fifth">
+    return this.state.simulation.map(s => (
+      <div className="column is-two-fifths">
         <Card>
           <header className="card-header">
             <p className="card-header-title">{s.plano}</p>
@@ -31,29 +32,41 @@ class FormResult extends Component {
           </div>
         </Card>
       </div>
-    )
-  }
+    ));
+  };
 
   render() {
-    const { ListCard } = this
+    const { ListCard } = this;
     return (
       <div>
-        <p className="title">Simulador FaleMais Telzir</p>
-        <p className="subtitle">
-          Valores da ligação:
-          <strong> ({this.props.form.origem}) 
-          para ({this.props.form.destino}) 
-          por {this.props.form.valor} min.</strong>
-        </p>
         <div className="columns">
-          <ListCard />
-        </div>
-        <div className="columns">
-          <div className="column is-one-fifth">
-            <Button onClick={this.props.restartSimulation}>Voltar</Button>
+          <div className="column is-half">
+            <p className="title">Simulador FaleMais Telzir</p>
+            <p className="subtitle">
+              Valores da ligação:
+              <strong>
+                ({this.props.form.origem}) para ({this.props.form.destino}) por{" "}
+                {this.props.form.valor} min.
+              </strong>
+            </p>
+            <div className="columns">
+              <ListCard />
+            </div>
+            <div className="columns">
+              <div className="column is-two-fifths">
+                <Button onClick={this.props.restartSimulation}>Voltar</Button>
+              </div>
+              <div className="column is-two-fifths">
+                <Button>Assinar plano</Button>
+              </div>
+            </div>
           </div>
-          <div className="column is-one-fifth">
-            <Button>Assinar plano</Button>
+          <div className="column is-half">
+            <img
+              src={this.image}
+              className="image-telzir"
+              alt="telefone telzir"
+            />
           </div>
         </div>
       </div>
@@ -61,10 +74,13 @@ class FormResult extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     form: state.form.form,
     simulation: state.form.simulation
-  }
-}
-export default connect(mapStateToProps,{restartSimulation})(FormResult)
+  };
+};
+export default connect(
+  mapStateToProps,
+  { restartSimulation }
+)(FormResult);
